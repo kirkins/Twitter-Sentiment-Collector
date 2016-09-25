ac67122198b8b1a70f3f5c8eef54b64f7a23303e
Philip Kirkbride <kirkins@gmail.com>
2016-09-24 21:28:04 -0400

    remove unneeded value from config

--- config/config.js
+++ config/config.js
@@ -5,7 +5,6 @@ config.TWITTER_CONSUMER_SECRET = '';
 config.TWITTER_ACCESS_TOKEN = '';
 config.TWITTER_ACCESS_TOKEN_SECRET = '';
 config.TWITTER_SEARCH_START_DATE = '2016-09-01';
-config.TWITTER_LAST_REPLIED = 743205419682775040; // don't search tweets older than last replied
-config.TWEET_FREQUENCY = 1; // search for a tweet ever 1 minute
+config.TWEET_FREQUENCY = 1; // minutes
 
 module.exports = config;
